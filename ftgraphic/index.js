bfe8be15a065ff62e4e6436018c4f0eb2e4148a0
Badger & Combes <95646286+BadgerMCR@users.noreply.github.com>
2023-01-30 15:48:21 +0000

    Add files via upload

--- ftgraphic/index.js
+++ ftgraphic/index.js
@@ -3,18 +3,41 @@ module.exports = {
 	description: "fortestingshittthings",
 	category: "Basic",
 	icon: "image",
-	hasEntries: false,
+	hasEntries: true,
 	props: [
-		{
-			name: "image2",
-			description: "Away Team",
-			type: "ImageFile"
-		},
 		{
 			name: "image",
 			description: "Home Team",
 			type: "ImageFile"
+		},
+		{
+			name: "image2",
+			description: "Away Team",
+			type: "ImageFile"
 		}
+	],
+	entry_props: [
+
+		{
+			name: "hn",
+			description: "Home Team",
+			type: "String"
+		},
+		{
+			name: "an",
+			description: "Away Team",
+			type: "String"
+		},
+		{
+			name: "score1",
+			description: "score 1",
+			type: "String"
+		},
+		{
+			name: "score2",
+			description: "score 2",
+			type: "String"
+		},
 	],
 	
 	vue_template: renderPug(getFile("template.pug").toString()),
